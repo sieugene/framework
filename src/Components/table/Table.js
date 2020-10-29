6322a2255f2fe4c7127a1c007833a5e1fd83765f
sieugene <evgenii.sidelnikov@unitbean.io>
2020-10-29 20:53:22 +0300

    table generator

--- src/Components/table/Table.js
+++ src/Components/table/Table.js
@@ -1,32 +1,8 @@
 import { ExcelComponent } from "./../../core/ExcelComponent";
+import { createTable } from "./table.template";
 export class Table extends ExcelComponent {
   static className = "excel__table";
   toHTML() {
-    return `  
-    <div class="row">
-      <div class="row-info"></div>
-      <div class="row-data">
-        <div class="column">A</div>
-        <div class="column">A</div>
-        <div class="column">A</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="row-info">1</div>
-      <div class="row-data">
-        <div class="cell selected" contenteditable>1</div>
-        <div class="cell" contenteditable>2</div>
-        <div class="cell" contenteditable>3</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="row-info">1</div>
-      <div class="row-data">
-        <div class="cell" contenteditable>1</div>
-        <div class="cell" contenteditable>2</div>
-        <div class="cell" contenteditable>3</div>
-      </div>
-    </div>
-  `;
+    return createTable();
   }
 }
---
+++ src/Components/table/table.template.js
@@ -0,0 +1,36 @@
+const CODES = {
+  A: 65,
+  Z: 90,
+};
+
+const createCell = () => {
+  return `<div class="cell" contenteditable></div>`;
+};
+const toColumn = (col) => {
+  return `<div class="column">${col}</div>`;
+};
+const createRow = (content, number) => {
+  const iterate = number !== undefined ? String(number) : "";
+  return `
+    <div class="row">
+    <div class="row-info">${iterate}</div>
+    <div class="row-data">
+    ${content}
+    </div>
+  </div>`;
+};
+
+const toChar = (_, char) => {
+  return String.fromCharCode(CODES.A + char);
+};
+export const createTable = (rowsCount = 15) => {
+  const colsCount = CODES.Z - CODES.A + 1;
+  const cols = new Array(colsCount).fill("").map(toChar).map(toColumn).join("");
+  const cells = new Array(colsCount).fill("").map(createCell).join("");
+  const rows = [];
+  rows.push(createRow(cols));
+  for (let i = 0; i < rowsCount; i++) {
+    rows.push(createRow(cells, i));
+  }
+  return rows.join("");
+};
