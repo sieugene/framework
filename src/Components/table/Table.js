42d2de241eae347624cdefb413018cb96230a568
sieugene <evgenii.sidelnikov@unitbean.io>
2020-11-09 19:44:25 +0300

    resize line, optimize rendering

--- src/Components/table/Table.js
+++ src/Components/table/Table.js
@@ -12,47 +12,50 @@ export class Table extends ExcelComponent {
     return createTable(20);
   }
   onMousedown(event) {
-    // console.log(event.target.getAttribute('data-resize'));
     if (event.target.dataset.resize) {
       console.log("start resizig", event.target.dataset.resize);
       const $resizer = $(event.target);
       const type = event.target.dataset.resize;
-      // const $parent = $resizer.$el.parentNode; //bad
-      //const $parent = $resizer.$el.closest('.column') //better but bad
       const $parent = $resizer.closest('[data-type="resizable"]');
-
       const coords = $parent.getCoords();
-      //кэшируем заранее querySelectorAll, чтобы на евенте не вызывать
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
-      $parent.$el.classList.add("active");
+      const ownProp = type === "col" ? "bottom" : "right";
+      let value;
+      $resizer.css({
+        opacity: 1,
+        [ownProp]: "-5000px",
+      });
 
       document.onmousemove = (e) => {
         if (type === "col") {
           const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
-          $parent.css({
-            width: `${value}px`,
+          value = coords.width + delta;
+          $resizer.css({
+            right: -delta + "px",
           });
         } else if (type === "row") {
           const delta = e.pageY - coords.bottom;
-          const value = coords.height + delta;
+          value = coords.height + delta;
+          $resizer.css({
+            bottom: `${-delta}px`,
+          });
+        }
+      };
+      document.onmouseup = () => {
+        document.onmousemove = null;
+        document.onmouseup = null;
+        $resizer.css({
+          opacity: 0,
+          bottom: 0,
+          right: 0,
+        });
+        if (type === "col") {
+          this.$root
+            .findAll(`[data-col="${$parent.data.col}"]`)
+            .forEach((el) => (el.style.width = value + "px"));
+        } else if (type === "row") {
           $parent.css({
             height: `${value}px`,
           });
-        }
-        //more renderings in mouseEvent!
-        //cells.forEach((el) => (el.style.width = value + "px"));
-      };
-      document.onmouseup = () => {
-        //очистка события
-        document.onmousemove = null;
-        if (type === "col") {
-          $parent.$el.classList.remove("active");
-          cells.forEach(
-            (el) => (el.style.width = $parent.$el.offsetWidth + "px")
-          );
-        } else if (type === "row") {
-          $parent.$el.classList.remove("active");
         }
       };
     }
