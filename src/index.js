295142e03959dd9506276c5a3f25d8d93cc23712
sieugene <evgenii.sidelnikov@unitbean.io>
2020-11-24 19:15:36 +0300

    base store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { Header } from "./Components/header/Header";
 import { Formula } from "./Components/formula/Formula";
 import { Tollbar } from "./Components/toolbar/Tollbar";
 import { Table } from "./Components/table/Table";
+import { createStore } from "./core/createStore";
+import { rootReducer } from "./store/rootReducer";
+
+const store = createStore(rootReducer);
 
 const excel = new Excel("#app", {
   components: [Header, Tollbar, Formula, Table],
+  store,
 });
-excel.render()
+excel.render();
 
 // console.log("Excel", excel);
