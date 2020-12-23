91a652a46daddfe701a4362dd1d166eaf5326a3b
sieugene <evgenii.sidelnikov@unitbean.io>
2020-12-23 19:41:04 +0300

    router test

--- src/core/routes/Router.spec.js
+++ src/core/routes/Router.spec.js
@@ -2,13 +2,28 @@ import { Router } from "./Router";
 import { Page } from "./../Page";
 
 class DashBoardPage extends Page {
+  $root = document.createElement("div");
+  param = "test";
+
   getRoot() {
-    const root = document.createElement("div");
-    root.innerHTML = "dashboard";
-    return root;
+    this.$root.innerHTML = "dashboard";
+    return this.$root;
+  }
+  destroy() {
+    this.param = undefined;
   }
 }
-class ExcelPage extends Page {}
+class ExcelPage extends Page {
+  $root = document.createElement("div");
+
+  getRoot() {
+    this.$root.innerHTML = "testing";
+    return this.$root;
+  }
+  destroy() {
+    this.$root.innerHTML = "";
+  }
+}
 
 describe("Router", () => {
   let router;
@@ -25,7 +40,16 @@ describe("Router", () => {
   });
 
   test("should render Dashboard Page", () => {
-    router.changePageHandler();
+    router.init();
     expect($root.innerHTML).toBe("<div>dashboard</div>");
+  });
+
+  test("should destroy Dashboard Page when page change on Excel Page", () => {
+    router.init();
+    expect(router.page.param).toBe("test");
+    window.location.hash = "#excel";
+    router.changePageHandler();
+    expect(router.page.param).toBe(undefined);
+    expect($root.innerHTML).toBe("<div>testing</div>");
   });
 });
